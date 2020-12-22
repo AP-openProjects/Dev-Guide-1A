e5590fdb0c8072f6b085db952c53dc3843904680
Luciano Riente <lucianotomasriente@gmail.com>
2020-12-22 16:16:03 -0300

    Se modificó la sección de 'about us'

---
+++ js/about-us.js
@@ -0,0 +1,20 @@
+document.getElementById("tomas-card").addEventListener("click", function () {
+  document.getElementById("name-dev").innerHTML = "TOMÁS NOBILE";
+  imageDev1.src = "img/Tomas.jpg";
+  document.getElementById("change-text").innerHTML =
+    "Tomás Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae ipsa omnis distinctio alias placeat elige  Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae ipsa omnis distinctio alias placeat eligendi recusandae";
+});
+
+document.getElementById("joustin-card").addEventListener("click", function () {
+  document.getElementById("name-dev").innerHTML = "JOUSTIN ORTÍZ";
+  imageDev1.src = "img/Joustin.jpg";
+  document.getElementById("change-text").innerHTML =
+    "Joustin Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae ipsa omnis distinctio alias placeat elige  Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae ipsa omnis distinctio alias placeat eligendi recusandae";
+});
+
+document.getElementById("luciano-card").addEventListener("click", function () {
+  document.getElementById("name-dev").innerHTML = "LUCIANO RIENTE";
+  imageDev1.src = "img/Luciano.jpg";
+  document.getElementById("change-text").innerHTML =
+    "Mi nombre es Luciano Tomás Riente. Al dia de la fecha, tengo los conocimientos básicos de C++, HTML, CSS y Javascript. Me encantaría poder aprender mucho más a lo largo de los años y en un futuro lograr trabajar en algún ambito de la programación.";
+});
