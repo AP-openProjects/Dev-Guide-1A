d057fbeaead174c37751c2fa276b2ca632e78181
Luciano Riente <lucianotomasriente@gmail.com>
2020-12-22 16:48:29 -0300

    Se agregaron scrolls reveal a la página

--- js/slide.js
+++ js/slide.js
@@ -1,10 +1,66 @@
-const navSlide= ()=>{
-    const burguer=document.querySelector('.burguer');
-    const nav= document.querySelector('.nav-links');
+const navSlide = () => {
+  const burguer = document.querySelector(".burguer");
+  const nav = document.querySelector(".nav-links");
 
-    burguer.addEventListener('click',()=> {
-        nav.classList.toggle('nav-active')
-    })
- }
+  burguer.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+  });
+};
 
 navSlide();
+
+window.sr = ScrollReveal();
+
+sr.reveal(".nav-bar", {
+  duration: 1700,
+  origin: "top",
+  distance: "100px",
+});
+
+sr.reveal(".dev-stories", {
+  duration: 1900,
+  origin: "left",
+  distance: "1000px",
+});
+
+sr.reveal(".about-us-h4", {
+  duration: 1900,
+  origin: "left",
+  distance: "1000px",
+});
+
+sr.reveal(".grid-devCards", {
+  duration: 1900,
+  origin: "left",
+  distance: "1000px",
+});
+
+sr.reveal(".container-header", {
+  duration: 1900,
+  origin: "left",
+  distance: "1000px",
+});
+
+sr.reveal(".grid-general", {
+  duration: 1900,
+  origin: "bottom",
+  distance: "200px",
+});
+
+sr.reveal(".container", {
+  duration: 1900,
+  origin: "left",
+  distance: "1000px",
+});
+
+sr.reveal(".iframe", {
+  duration: 1900,
+  origin: "left",
+  distance: "1000px",
+});
+
+sr.reveal(".footer", {
+    duration: 1700,
+    origin: "bottom",
+    distance: "500px",
+  });
