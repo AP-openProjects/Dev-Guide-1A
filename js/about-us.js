d4a2bd7aa80f74a22a1329edae685840bd6200f2
Luciano Riente <lucianotomasriente@gmail.com>
2021-01-15 16:12:43 -0300

    Se agregan las redes sociales de los desarrolladores y de la comúnidad

--- js/about-us.js
+++ js/about-us.js
@@ -3,6 +3,8 @@ document.getElementById("tomas-card").addEventListener("click", function () {
   imageDev1.src = "img/Tomas.jpg";
   document.getElementById("change-text").innerHTML =
     "Mi nombre es Tomás Nobile, tengo 21 años y estudio desarrollo web de una manera autodidacta. Poseo conocimientos en tecnologías orientadas al frontend y backend de sitios web.";
+  document.getElementById("social-link-github").href = "https://github.com/tomas-nobile";
+  document.getElementById("social-link-linkedin").href = "https://www.linkedin.com/in/tom%C3%A1s-ezequiel-nobile-4511111a9/";
 });
 
 document.getElementById("joustin-card").addEventListener("click", function () {
@@ -10,6 +12,8 @@ document.getElementById("joustin-card").addEventListener("click", function () {
   imageDev1.src = "img/Joustin.jpg";
   document.getElementById("change-text").innerHTML =
     "Mi nombre es Joustin Ortiz, tengo 13 años (se que es algo temprano para empezar) me empezó a interesar todo el tema del desarrollo desde hace mucho tiempo,  pero es desde hace un mes el cual empece a estudiar los fundamentos de la programación. En futuro, me gustaría trabajar programando.";
+  document.getElementById("social-link-github").href = "https://github.com/MrJoustinOF";
+  document.getElementById("social-link-linkedin").href = "#";
 });
 
 document.getElementById("luciano-card").addEventListener("click", function () {
@@ -17,4 +21,6 @@ document.getElementById("luciano-card").addEventListener("click", function () {
   imageDev1.src = "img/Luciano.jpg";
   document.getElementById("change-text").innerHTML =
     "Mi nombre es Luciano Tomás Riente. Al dia de la fecha, tengo los conocimientos básicos de C++, HTML, CSS y Javascript. Me encantaría poder aprender mucho más a lo largo de los años y en un futuro lograr trabajar en algún ambito de la programación.";
+  document.getElementById("social-link-github").href = "https://github.com/Lucssiano";
+  document.getElementById("social-link-linkedin").href = "https://www.linkedin.com/in/luciano-riente-7551901b1/";
 });
